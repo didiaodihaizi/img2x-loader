36ca70f9dc7770c5010a155818f302f113e94cf2
xuzhihao <xuzhihao@cmcm.com>
2019-11-04 01:40:22 +0800

    推送源码

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = function (source) {
+	const imgs = source.match(/(background|background-image)\:+[\s*]+url\((\S*)\)?__2x\)[\s\w-]*\;/g)
+    let str = source
+    if (imgs && imgs.length) {
+        imgs.forEach(item => {
+            let fix = item.match(/\.(\S*)\?/)[1]
+            let name = item.match(/\((\S*)\./)[1]
+            let afterReplace = `${item}background-image: image-set(url(${name}.${fix}) 1x, url(${name}@2x.${fix}) 2x);`
+            str = str.replace(item, afterReplace)
+        })
+    }
+	this.callback(null, str)
+}
