da4d86e7b03214539ab7667b4d01249e784ba717
xuzhihao <xuzhihao@cmcm.com>
2019-11-13 19:58:13 +0800

    fix:修改正则判断问题

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = function (source) {
-	const imgs = source.match(/(background|background-image)\:+[\s*]+url\((\S*)\)?__2x\)[\s\w-]*\;/g)
-    let str = source
+	const imgs = source.match(/(background|background-image)\:\s*url\((\S*)\)?__2x\)[\s\w-]*\;/g)
+	let str = source
     if (imgs && imgs.length) {
         imgs.forEach(item => {
-            let fix = item.match(/\.(\S*)\?/)[1]
+            let fix = item.match(/\w\.(\S*)\?/)[1]
             let name = item.match(/\((\S*)\./)[1]
             let afterReplace = `${item}background-image: image-set(url(${name}.${fix}) 1x, url(${name}@2x.${fix}) 2x);`
             str = str.replace(item, afterReplace)
